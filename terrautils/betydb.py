97a572fa939d432d3c7d6a6eab0b22d7c39f465f
Max Burnette <mburnet88@gmail.com>
2017-05-17 15:51:00 -0500

    Add BETYdb plot querying

--- terrautils/betydb.py
+++ terrautils/betydb.py
@@ -3,27 +3,108 @@
 This module provides wrappers to BETY API for getting and posting data.
 """
 
-def get_experiment(date):
-    """
-    """
-    pass
+import logging
+import requests
 
-def get_plot(bbox):
-    """
-    """
-    pass
+from osgeo import ogr
+
+
 
 def get_cultivar(plot):
     """
     """
     pass
 
-def trigger_extraction():
+
+def get_experiment(date):
     """
     """
     pass
 
-def submit_traits(csv):
+
+def get_plot(bbox):
     """
     """
     pass
+
+
+def get_sites(host="https://terraref.ncsa.illinois.edu/bety", city=None, sitename=None, contains=None):
+    """Get list of sites from BETYdb, filtered by city or sitename prefix if provided.
+
+        e.g.
+            get_sites(city="Maricopa")
+            get_sites(sitename="MAC Field Scanner Season 2")
+
+    host -- URL of BETYdb instance to query
+    city -- city parameter to pass to API
+    sitename -- string to filter returned sites by sitename prefix
+    contains -- (lat, lon) tuple; only sites that contain this point will be returned
+    """
+    sess = requests.Session()
+    sess.auth = ("guestuser", "guestuser")
+
+    betyurl = host + "/sites.json"
+    if city:
+        betyurl += "?city=%s" % city
+
+    r = sess.get(betyurl)
+    r.raise_for_status()
+
+    # Filter some results on client side if necessary
+    if sitename or contains:
+        all_results = r.json()
+        filtered = []
+        if contains:
+            targgeom = ogr.CreateGeometryFromWkt("POINT (%s %s 0)" % (contains[0], contains[1]))
+
+        removed = 0
+        for res in all_results:
+            if 'site' in res:
+                currsite = res['site']
+            else:
+                removed += 1
+                continue
+
+            # Reject sites that don't begin with sitename, if provided
+            if sitename and 'sitename' in currsite:
+                if currsite['sitename'] == None or not currsite['sitename'].startswith(sitename):
+                    removed += 1
+                    continue
+
+            # Reject sites that don't intersect contains, if provided
+            elif contains and 'geometry' in currsite:
+                if currsite['geometry'] == None:
+                    removed += 1
+                    continue
+                else:
+                    sitegeom = ogr.CreateGeometryFromWkt(currsite['geometry'])
+                    intersection = targgeom.Intersection(sitegeom)
+                    if str(intersection) == 'GEOMETRYCOLLECTION EMPTY':
+                        removed += 1
+                        continue
+
+            filtered.append(res)
+        print("filtered %s sites" % removed)
+        return filtered
+
+    else:
+        return r.json()
+
+
+def submit_traits(csv, betykey, betyurl="https://terraref.ncsa.illinois.edu/bety/api/beta/traits.csv"):
+    """Submit a CSV containing traits to the BETYdb API.
+
+    csv -- CSV to submit
+    betykey -- API key for given BETYdb instance
+    betyurl -- URL (including /api portion) to submit CSV to
+    """
+    sess = requests.Session()
+
+    r = sess.post("%s?key=%s" % (betyurl, betykey),
+                  data=file(csv, 'rb').read(),
+                  headers={'Content-type': 'text/csv'})
+
+    if r.status_code == 200 or r.status_code == 201:
+        logging.info("...CSV successfully uploaded to BETYdb.")
+    else:
+        logging.error("Error uploading CSV to BETYdb %s" % r.status_code)
