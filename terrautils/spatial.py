fc6e6a31511372fb067f41afd2510f7e0246a6ba
Max Burnette <mburnet88@gmail.com>
2017-09-21 14:08:52 -0500

    add new coordinates

--- terrautils/spatial.py
+++ terrautils/spatial.py
@@ -110,19 +110,19 @@ def calculate_gps_bounds(metadata, sensor="stereoTop"):
         # TODO: These constants should live in fixed metadata once finalized
         if scandirection == 0: # Negative scan
             west_position = ( float(gantry_x) + float(cambox_x) + 0.082,
-                              float(gantry_y) + float(2*float(cambox_y)) - scan_distance/2 - 4.263, #Might be less than this
+                              float(gantry_y) + float(2*float(cambox_y)) - scan_distance/2 - 3.763, #Might be less than this
                               float(gantry_z) + float(cambox_z) )
 
             east_position = ( float(gantry_x) + float(e_cambox_x) + 0.082,
-                              float(gantry_y) + float(2*float(e_cambox_y)) - scan_distance/2 - 0.046,
+                              float(gantry_y) + float(2*float(e_cambox_y)) - scan_distance/2 + 0.454,
                               float(gantry_z) + float(e_cambox_z) )
         else: # Positive scan
             west_position = ( float(gantry_x) + float(cambox_x) + 0.082,
-                              float(gantry_y) + float(2*float(cambox_y)) + scan_distance/2 - 3.23,
+                              float(gantry_y) + float(2*float(cambox_y)) + scan_distance/2 - 4.23,
                               float(gantry_z) + float(cambox_z) )
 
             east_position = ( float(gantry_x) + float(e_cambox_x) + 0.082,
-                              float(gantry_y) + float(2*float(e_cambox_y)) + scan_distance/2 - 1.44,
+                              float(gantry_y) + float(2*float(e_cambox_y)) + scan_distance/2 + 0.4,
                               float(gantry_z) + float(e_cambox_z) )
 
         east_gps_bounds = _get_bounding_box_with_formula(east_position, [fov_x, fov_y])
