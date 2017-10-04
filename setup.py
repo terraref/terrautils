aeb053a0c7fcdf7ba9112aa40c8b2d338c14ba9b
Max Burnette <mburnet88@gmail.com>
2017-10-04 10:49:08 -0500

    move scan_programs and adjust import

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 setup(name='terrautils',
       version='1.0.0',
       packages=['terrautils'],
+      include_package_data=True,
       install_requires=[
           'utm', 
           'python-dateutil',
