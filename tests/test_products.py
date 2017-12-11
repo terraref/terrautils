37de586cb554f183fac1c5b9876e8c777e765ff1
Jeff Terstriep <jefft@illinois.edu>
2017-12-11 21:59:11 +0000

    Improve handling of environmental variables need for testing.

--- tests/test_products.py
+++ tests/test_products.py
@@ -1,34 +1,52 @@
 import os
-from terrautils.files import *
+import pytest
+from terrautils.products import *
 
-HOST = os.environ.get('CLOWDER_URL')
-KEY = os.environ.get('CLOWDER_KEY')
 PLOT = 'MAC Field Scanner Season 1 Field Plot 101 W'
 SENSOR = 'Thermal IR GeoTIFFs Datasets'
 
-def test_sensor_list():
-    l = get_sensor_list(None, HOST, KEY)
+
+# TODO This is testing against a live Clowder instance. It might be
+# better to use some sort of mocking.
+@pytest.fixture()
+def clowder(scope='session'):
+    """Return Clowder url and key for testing.
+
+    The url and key are retreived from env vars. If they are not avaiable
+    testing is aborted.
+    """
+    url = os.environ.get('CLOWDER_URL', '')
+    key = os.environ.get('CLOWDER_KEY', '')
+    if not url or not key:
+        pytest.exit('Tests require CLOWDER_URL and CLOWDER_KEY to be set.')
+    yield (url, key)
+
+
+def test_sensor_list(clowder):
+    url, key = clowder
+    l = get_sensor_list(None, url, key)
     assert l
 
-def test_get_sensor():
-    s = get_sensor(None, HOST, KEY, SENSOR)
+def test_get_sensor(clowder):
+    url, key = clowder
+    s = get_sensor(None, url, key, SENSOR)
     assert s
 
-def test_get_sensor_sitename():
-    s = get_sensor(None, HOST, KEY, SENSOR, PLOT)
+def test_get_sensor_sitename(clowder):
+    url, key = clowder
+    s = get_sensor(None, url, key, SENSOR, PLOT)
     assert s
 
-def test_file_listing():
-    l = get_file_listing(None, HOST, KEY, SENSOR, PLOT)
+def test_file_listing(clowder):
+    url, key = clowder
+    l = get_file_listing(None, url, key, SENSOR, PLOT)
+
     assert l
     assert len(l)
-    with open('/tmp/l', 'w') as f:
-        f.write(str(l[0]))
 
-def test_file_paths():
+def test_file_paths(clowder):
+    url, key = clowder
     paths = extract_file_paths(
-            get_file_listing(None, HOST, KEY, SENSOR, PLOT))
+            get_file_listing(None, url, key, SENSOR, PLOT))
     assert paths
-    with open('/tmp/p', 'w') as f:
-        f.write(str(paths[0]))
     
